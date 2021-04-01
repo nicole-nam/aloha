c547b45629c0c58dc1ccd7a663cdd074b15f817c
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-04-01 09:46:53 -0400

    chore initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let elem = document.querySelector(".carousel");
+
+  let flkty = new Flickity(elem, {
+    wrapAround: true,
+    cellAlign: "left",
+    contain: true,
+    freeScroll: true,
+  });
+
+  document.getElementById("subBtn").addEventListener("click", function (e) {
+    const input = document.getElementById("fname").value;
+    if (input.indexOf("@") !== -1) {
+      alert("Thank you for subcribing!");
+    } else {
+      alert("This is not a valid email address.");
+    }
+  });
+
+  const cart = document.getElementsByClassName("cartBtn");
+  let numClick = 0;
+  for (let i = 0; i < cart.length; i++) {
+    cart[i].addEventListener("click", function () {
+      numClick++;
+      console.log(numClick);
+      document.getElementById("number").style.display = "block";
+      document.getElementById("number").innerHTML = numClick;
+    });
+  }
+
+  // //sticky navigation
+
+  window.onscroll = function () {
+    myFunction();
+  };
+
+  const navigation = document.getElementById("nav");
+
+  var sticky = navigation.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navigation.classList.add("sticky");
+    } else {
+      navigation.classList.remove("sticky");
+    }
+  }
+});
